9a4b1cc28e45bc826408bebfcd7164c743197a83
zDany01 <85403430+zDany01@users.noreply.github.com>
2022-10-16 19:18:07 +0200

    Project Setup
    Added C# Project to repository
    Added AssemblyInfo
    Added Telegram.Bot package

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramMessageAlertBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
